ad15096da8abf6694a20552a6c004e2e41bee03f
Dana Mulyana <danabontot@gmail.com>
2021-08-16 20:09:48 +0700

    belajar membuat helloworld

---
+++ helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello world")
+}
+
+// menjalankan compile golang di cmd
+// go build helloworld.go
+// menjalnkan tanpa compile
+// go run helloworld.go
