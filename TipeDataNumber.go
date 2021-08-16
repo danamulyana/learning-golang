a0d4addf7249ac256b9c7023bb4831c6fc57a542
Dana Mulyana <danabontot@gmail.com>
2021-08-16 21:42:19 +0700

    Tipe data Number

---
+++ TipeDataNumber.go
@@ -0,0 +1,37 @@
+// tipe data integer
+// 
+// --- 1 ---
+// int8
+// int16
+// int32
+// int64
+// --- 2 ---
+// uint8
+// uint16
+// uint32
+// uint64
+
+// tipe data floating point
+// 
+// float32
+// float64
+// complex64
+// complex128
+
+// alias
+// 
+// tipedata | alias untuk
+// byte 	| uint8
+// rune 	| int32
+// int 		| Minimal int32
+// uint 	| Minimal uint32
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("satu = ", 1)
+	fmt.Println("dua = ", 2)
+	fmt.Println("tiga koma lima = ", 3.5)
+}
